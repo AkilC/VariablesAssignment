68c4e259c88905d6a0cb18578ca04cc6d7810c54
AkilC <cooperakil@gmail.com>
2020-08-31 13:21:20 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int test = 99;
+	float price = 24.99f;
+	String name = "Akil Cooper";
+	System.out.println("Test Score: "+ test);
+	System.out.println("Price tag: "+ price);
+	System.out.println("Username: "+ name);
+    }
+}
